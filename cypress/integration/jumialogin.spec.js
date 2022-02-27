2de2d4543df7720cccca386b744c50b7934a5a7e
Ememtobi <ememoloruntobi@gmail.com>
2022-02-27 14:03:06 +0100

    second

---
+++ cypress/integration/jumialogin.spec.js
@@ -0,0 +1,13 @@
+/// <reference types= "cypress" />
+
+
+//This function is to describe the test suite
+describe("Jumia Login Test suite", function () {
+    
+    //This function is to describe the test case
+    it(`Jumia login`, function () {
+    //Visit instagram.
+    cy.visit("https://www.jumia.com.ng/customer/account/login/");
+   
+  });
+});
---
+++ cypress/integration/logininstagram.spec.js
@@ -0,0 +1,17 @@
+/// <reference types= "cypress" />
+
+
+//This function is to describe the test suite
+describe("Instagram Login Test suite", function () {
+    
+    //This function is to describe the test case
+    it(`Instagram login`, function () {
+    //Visit instagram.
+    cy.visit("https://www.instagram.com/");
+
+    cy.get(':nth-child(1) > ._9GP1n > .f0n8F > ._2hvTZ').type('tree');
+
+    cy.get(':nth-child(2) > ._9GP1n > .f0n8F > ._2hvTZ').type('tree123');
+   
+  });
+});
